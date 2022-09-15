3d5f24bd53353485768ff6e1b1ab8e8cd16fd932
abu bokor suraj <abusurajbd@gmail.com>
2022-09-15 19:42:15 +0600

    NgFor

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Book{
+  name: string;
+  author: string;
+  src: string;
+}
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-name: string = "Clean Code";
-author: string = "Robert C Martin"
-src: string = "https://images-eu.ssl-images-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_ML2_.jpg" ;
-name2: string = "Pragmatic Programmer";
-author2: string = " David Thomas "
-src2: string = "https://m.media-amazon.com/images/P/B07VRS84D1.01._SCLZZZZZZZ_SX500_.jpg" ;
+
+  books:Book[] = [
+  {name:  "Clean Code",
+  author: "David Thomas",
+  src: "https://images-eu.ssl-images-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_ML2_.jpg", },
+  {name:  "Pragmatic Programmer",
+  author: "Robert C Martin",
+  src: "https://m.media-amazon.com/images/P/B07VRS84D1.01._SCLZZZZZZZ_SX500_.jpg" }
+];
 
 myName: string = "";
 isShowing: boolean = true;
